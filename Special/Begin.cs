15a5c42f4f0c42ed5425abdbe9e4d7ca6aa5a3ce
Hunter Crossett <hunter.crossett@gmail.com>
2015-10-10 17:23:41 -0500

    Finished begin

--- Special/Begin.cs
+++ Special/Begin.cs
@@ -1,20 +1,25 @@
-// Begin -- Parse tree node strategy for printing the special form begin
-
+//Hunter Crossett and Emmitt Bush
 using System;
-
 namespace Tree
 {
     public class Begin : Special
     {
-        // TODO: Add any fields needed.
- 
-        // TODO: Add an appropriate constructor.
 	public Begin() { }
-
         public override void print(Node t, int n, bool p)
         {
-            // TODO: Implement this function.
+            for (int i = 0; i < n; i++)
+			    Console.Write(' ');
+            if (!p) //bool is for checking RPAREN is written
+            {
+                Console.Write("("); 
+                p = true;
+            }
+            Console.WriteLine("begin"); //print out keywords
+            if (t.getCdr().isPair())            //probably should throw an error if it isnt.
+                t.getCdr().print(n + 2, true); 
+		for (int i = 0; i < n; i++) 
+            Console.Write(' ');
+		Console.Write(")");
         }
     }
 }
-
