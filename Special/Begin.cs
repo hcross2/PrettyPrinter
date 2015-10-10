1df61294fb0e6e7815b11aef58fe51c267e40bb5
Hunter Crossett <hunter.crossett@gmail.com>
2015-10-10 17:39:54 -0500

    Revert "More begin updates"
    This reverts commit 22f10b3c4e5edc7c3a119129cdb80b2430e42c58.

--- Special/Begin.cs
+++ Special/Begin.cs
@@ -5,30 +5,14 @@ namespace Tree
     public class Begin : Special
     {
 	public Begin() { }
-        public override void print(Node t, int n, bool p, bool q) //when is this q passed in?
+        public override void print(Node t, int n, bool p)
         {
             for (int i = 0; i < n; i++)
 			    Console.Write(' ');
-            if (!p) //bool is for checking LPAREN is written
+            if (!p) //bool is for checking RPAREN is written
             {
                 Console.Write("("); 
                 p = true;
-            }
-            if (!q) //bool is for LPAREN for statements
-            {
-                Console.WriteLine();
-                int o = n + 4; //indent!
-                Node cdr = t.getCdr(); //the rest!
-                while (cdr.isPair()) 
-                {
-                    cdr.getCar().print(o, p, q)
-                    Console.WriteLine();
-                    cdr = cdr.getCdr(); 
-                }
-                //what if it is not a correct list? check for it? idk
-                for (int i = 0; i < n; i++)
-                    Console.Write(' ');
-                Console.Write(")"); //finished the begin
             }
             Console.WriteLine("begin"); //print out keywords
             if (t.getCdr().isPair())            //probably should throw an error if it isnt.
