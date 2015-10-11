3f08ad2a206a6b9adb6a868afb26d8983a4c5d48
Hunter Crossett <hunter.crossett@gmail.com>
2015-10-11 10:47:07 -0500

    working on define

--- Special/Define.cs
+++ Special/Define.cs
@@ -7,7 +7,17 @@ namespace Tree
 	public Define() { }
         public override void print(Node t, int n, bool p)
         {
+            if (!p)
+            {
+                for (int i = 0; i < n; i++)
+                    Console.Write(' ');
+                Console.Write("(");
+                p = true;
+            }
+            Console.Write("define");
+            n = 1; 
             
+            t.cdr.print(n, p);
         }
     }
 }
