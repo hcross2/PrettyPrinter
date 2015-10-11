2ef79d9aeefa6fe10c22c0252b815d198a595c3d
Hunter Crossett <hunter.crossett@gmail.com>
2015-10-11 11:55:11 -0500

    shell of IF started

--- Special/If.cs
+++ Special/If.cs
@@ -7,7 +7,33 @@ namespace Tree
 	public If() { }
         public override void print(Node t, int n, bool p)
         {
+            for (int i = 0; i < n; i++)
+                    Console.Write(' ');
+            if (!p)
+            {
+                Console.Write("(");
+                p = true;
+            }
+            Console.Write("if");
             
+            if (t.cdr.isPair() & t.cdr.cdr.isPair() & t.cdr.cdr.cdr.isPair() & !t.cdr.cdr.cdr.cdr.isPair()); //shits ok
+            {
+                n = 1;
+                Node identifier = t.getCdr().getCar();
+                if (indentifier.isSymbol()) //WHAT IF THIS IS A LIST!?!?!?
+                    identifier.print(n, p);
+                else
+                    Console.WriteLine("IF ERROR: DEFINE REQUIRES AN IDENTIFIER AFTER DEFINE");
+                identifier = t.getCdr().getCdr();
+                while (identifier.isPair())
+                {
+                    identifier.car.print(1, p); //assumes print recursively prints items (including RPAREN)
+                    identifier = identifier.getCdr();
+                }
+            }
+            else //shits broke
+                Console.WriteLine("IF ERROR: INCORRECT SYNTAX");
+            Console.Write(")");
         }
     }
 }
